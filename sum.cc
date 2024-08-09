8b99c4748e8b5fa890968e1ee93a7adbdd6838e3
acalanato <vagner.assistencia@gmail.com>
2024-08-09 19:32:10 -0300

    Done

---
+++ sum.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, sum;
+  cout << "Input a number: ";
+  std::cin >> a;
+  cout << "Another: ";
+  std::cin >> b;
+
+  sum = a + b;
+  cout << "Sum:" << sum << endl;
+}
