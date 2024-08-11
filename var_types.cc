238d71fc92f38c8605388f27df246f162761fbf5
acalanato <vagner.assistencia@gmail.com>
2024-08-10 21:02:58 -0300

    Done

--- var_types.cc
+++ var_types.cc
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main() {
+void stringss() {
+  string mystring;
+  mystring = "Anything is possible";
+  cout << mystring << endl;
+}
+
+void declaration() {
+  int foo = 0;
+  auto bar = foo;
+  decltype(foo) tar;
+}
+
+void sumsum() {
+  int a, b, result;
+  a = 5;
+  b = 2;
+  a = a + 1;
+  result = a - b;
+  cout << result << endl;
+}
+
+void sum() {
   int a = 5;
   int b = 2;
   a += 1;
   int result = a - b;
-  std::cout << result << std::endl;
+  cout << result << std::endl;
+}
+
+int main() {
+  sum();
+  sumsum();
+  stringss();
 }
