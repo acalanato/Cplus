a19935991dfe7c833eec9cb5beae3de6420963f4
acalanato <vagner.assistencia@gmail.com>
2024-08-11 09:33:29 -0300

    Initial commit

---
+++ constants.cc
@@ -0,0 +1,11 @@
+#import <iostream>
+
+int main() {
+  int dec = 75;
+  int oct = 0113;
+  int hex = 0x4b;
+  float pi = 3.14159;
+  float avogadro = 6.02e23;
+  float eCharge = 1.6e-19;
+  float three = 3.0;
+}
