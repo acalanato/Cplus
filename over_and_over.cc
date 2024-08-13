2f8d97fadd3acb3ef64468fb4cb12513bfd3e2da
acalanato <vagner.assistencia@gmail.com>
2024-08-13 19:23:12 -0300

    Tests

--- over_and_over.cc
+++ over_and_over.cc
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+template <class Teste>
+Teste sum (Teste a, Teste b) {
+  return a + b;
+}
+
 int operate (int a, int b) {
   return (a * b);
 }
@@ -9,10 +14,24 @@ double operate (double a, double b) {
   return (a / b);
 }
 
+int sum (int a, int b) {
+  return a + b;
+}
+
+double sum (double a, double b) {
+  return a + b;
+}
+
 int main () {
+
+  cout << sum<int>(14,16) << '\n';
   int x=5,y=2;
   double n=5.0,m=2.0;
   cout << operate (x,y) << '\n';
   cout << operate (n,m) << '\n';
+
+  cout << sum (10, 20) << '\n';
+  cout << sum (1.1, 1.2) << '\n';
+
   return 0;
 }
